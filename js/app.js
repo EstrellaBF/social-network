bdd28be9ec1cd3d3f29238a5c65bbd1e1d28d91a
EstrellaBF <stern.von.nord>
2018-01-08 17:41:16 -0500

    editando index y app

--- js/app.js
+++ js/app.js
@@ -1,1 +1,11 @@
-alert('hola');
+$(document).ready(function() {
+// Si se hace click
+  $('body').on('click', function() {
+    window.location.href = 'views/register.html';
+  });
+  
+  // Si no se hace click, igual se ejecutará la página restaurant
+  setTimeout(function() {
+    window.location.href = 'views/register.html';
+  }, 5000);
+});
