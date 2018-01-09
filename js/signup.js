2cb2f8e91cffbe552e45aad2d0514ecff39e28a4
EstrellaBF <stern.von.nord>
2018-01-08 23:23:14 -0500

    intentando jalar el password

--- js/signup.js
+++ js/signup.js
@@ -1,17 +1,19 @@
 $('document').ready(function() {
   // Se crea variables para cada div y se filtrará por find ya que en clase explicaron que de esta forma evitaremos cargar de muchas cosas la página y se hace ligera
+  // Ecxcepto los que tendrán eventos
   var $signup = $('#signup');
   var $photoChrome = $('#photo-chrome');
   var $password = $('#password');
   var $passwordInput = $('#password input');
   var $signupBox = $('#signup-box');
+  var $signupBoxButton = $('#signup-box button');
 
   // Ocultando input de password y la etiuqeta p de bienvenida
   $password.hide();
   $signupBox.find('p').hide();
 
   // desactivando boton de registro
-  $signupBox.find('button').prop('disabled', true);
+  $signupBoxButton.prop('disabled', true);
 
   // FIREBASE
   // Initialize Firebase
@@ -40,52 +42,44 @@ $('document').ready(function() {
     });   
   });
 
-  // Se limita la contraseña entre 4 y 10 dígitos
-  $passwordInput.on('input', function() {
+  // objeto de la base de datos
+  // creando var para almacenar del usuario su nombre mail y foto
+  var userInfo = {};
+
+  // Evento que limita la contraseña entre 4 y 10 dígitos
+  $passwordInput.on('input', passwordLength);
+
+  // función que limita la contraseña entre 4 y 10 dígitos
+  function passwordLength() {
     // console.log($(this).val());
     // console.log($(this).val().length);
     if ($(this).val().length >= 4 && $(this).val().length <= 10) {
-      $signupBox.find('button').prop('disabled', false);
+      $signupBoxButton.prop('disabled', false);
+      localStorage.password = $(this).val();
+      // localStorage.phoneNumber = $(this).val();
     } else {
-      $next.prop('disabled', true);
-      $messageSMS.text('');
+      $signupBoxButton.prop('disabled', true);
     };
-  });
-  
-  // Evento para poder confirmar la cantidad de números que se ingresa.
-  // Dígitos serán entre 9 o 10, 9 para Perú y 10 como máximo como se pide en el LMS
-  // $input.on('input', function () {
-  //   if ($(this).val().length >= 9 && $(this).val().length <= 10) {
-  //     localStorage.phoneNumber = $(this).val();
-  //     $messageSMS.text('We\'ll send a text to verify your phone');
-  //     $buttonSendSms.show().css({ 'background-color': 'green', 'color': '#fff' });
-  //   } else {
-  //     $next.prop('disabled', true);
-  //     $messageSMS.text('');
-  //   };
-  // });
-
-
+  }
   // Guardando datos 
   function saveAccount(user) {
-    // creando var para almacenar del usuario su nombre mail y foto
-    var userInfo = {
-      // cuando se inicia sesion, es un unico numer
-      uid: user.uid,
-      nombre: user.displayName,
-      correo: user.email,
-      foto: user.photoURL
-    };
+    userInfo.uid = user.uid;
+    userInfo.nombre = user.displayName;
+    userInfo.correo = user.email;
+    userInfo.foto = user.photoURL;
+    // userInfo.nombrecito = getPassword;
     // guardando en firebase, recuerda que set grabaría en toda la rama, osea se sustituiria. Se concatena para que se almacene en la misma user id
     firebase.database().ref('newDB/' + user.uid).set(userInfo); 
   }
+      console.log(userInfo);
+  console.log(localStorage.password);
 
-  // // Jalando a todos los usuarios de la BD
-  // firebase.database().ref('newDB').on('child_added', function(s) {
-  //   var user = s.val();   
-  //   $('#amigos-foto').append('<img width=\'100px\' src=\''+ user.foto +'\'/>', '<p> '+user.nombre+ '<p/>');
-    
-  // })
+
+  // Guardando todos los datos al hacer click
+  // $signupBoxButton.on('click', function() {
+  // });
+
+  
   // END FIREBASE
 
 
