7c0290fc58446bc3409456831c8da637a39e7872
betsy18 <betsy_dessire@hotmail.com>
2018-01-11 15:44:35 -0500

    Validando que no acepte campos vacios

--- js/news.js
+++ js/news.js
@@ -2,9 +2,13 @@ $(document).ready(function() {
   $('#share-btn').click(function() {
     event.preventDefault();
     var $post = $('#input-post').val();
-    // agregando los post
-    $('#new-post').prepend('<div class="col-xs-10 col-xs-offset-1"><p>' + $post + '</p></div>');
-    
-    $('#input-post').val('');
+    // validando para que no acepte campos vacios
+    if ($post.length > 1) {
+      // agregando los post
+      $('#new-post').prepend('<div class="col-xs-10 col-xs-offset-1"><p>' + $post + '</p></div>');
+      $('#input-post').val('');
+    } else {
+      alert('No puedes ingresar campos vacíos');
+    }
   });
 });
--- js/profile.js
+++ js/profile.js
@@ -1,17 +1,24 @@
 $(document).ready(function() {
   var $imageProfile = $('#image-profile');
-  
+  // photo que se debe de capturar desde la computadora
+  var $photo = $('#capture');
+  // imagen para capturar desde la computadora
+  var $image = $('#input-file');
+
+  // instanciando el objeto
+  var $newDB = firebase.database();
+  var dataReference = $dataBase.ref('newDB');
+  console.log(dataReference);
+
   $('#share-btn').click(function() {
     event.preventDefault();
     var $post = $('#input-post').val();
-    // agregando los post
-    $('#new-post').prepend('<div class="col-xs-10 col-xs-offset-1"><p>' + $post + '</p></div>');
-    
-    // var $input = $('#input-file');
-    // console.log($input);
-    console.log($('#capture').val());
-    // si toma una imagen subirla al comparir
-    
-    $('#input-post').val('');
+    if ($post.length > 1) {
+      $('#new-post').prepend('<div class="col-xs-10 col-xs-offset-1"><p>' + $post + '</p></div>');
+      console.log($('#capture').val());
+      $('#input-post').val('');
+    } else {
+      alert('No puedes ingresar campos vacíos');
+    }
   });
 });
