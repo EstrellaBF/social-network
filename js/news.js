24047f2999fa0d9862b9a93fd6e2763c703a2290
AndreEG <andrea_206_93@hotmail.com>
2018-01-15 10:17:40 -0500

    corrige estilos desktop

--- js/news.js
+++ js/news.js
@@ -2,9 +2,12 @@ $(document).ready(function() {
   $('#share-btn').click(function() {
     event.preventDefault();
     var $post = $('#input-post').val();
+    var $moment = moment().format('LLL');
     // agregando los post
-    $('#new-post').prepend('<div class="col-xs-10 col-xs-offset-1"><p>' + $post + '</p></div>');
-    
+    if ($post.length > 0) {
+      $('#new-post').prepend('<div class="col-xs-10 col-xs-offset-1 div-post"><p><strong>' + 'Name & Last name' + '</strong></p><p>' + $post + '</p><p>' + $moment + '</p></div>');
+    }
+    // limpiando el input
     $('#input-post').val('');
   });
 });
